c114e23a9fbee7dae9a8b6b28cb61b4de5efd14d
Ivana <ivaptt@gmail.com>
2021-12-28 21:31:22 +0100

    created and added a new logo and fixed Header for HomePage

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 import { Dino } from "../../App";
-import tr from "./tr.svg";
+import logo from "./logo.png";
 
 function Header() {
   const properties = useContext(Dino);
@@ -15,34 +15,29 @@ function Header() {
   return (
     <div className="header">
       <Link to="/">
-        <img className="header-logo" src={tr} alt="logo"></img>
+        <img className="header-logo" src={logo} alt="logo"></img>
       </Link>
 
-
-     
       {properties.token === null ? (
         <Link to="/login">
           <button className="login-button">Login</button>
         </Link>
-      ) : ( <div className="header-buttons">
-       <div className="report-buttons">
-       <Link to='/reportpage'>
-        <button className="report-button">Reports</button>
-        </Link>
+      ) : (
+        <div className="header-buttons">
+          <div className="report-buttons">
+            <Link to="/reportpage">
+              <button className="report-button">Reports</button>
+            </Link>
 
-        <Link to='/createreport'>
-        <button className="report-button">CreateReport</button>
-        </Link>
-        </div>
-        <Link to="/">
-          <button onClick={logout}>Logout</button>
-        </Link>
+            <Link to="/createreport">
+              <button className="report-button">CreateReport</button>
+            </Link>
+          </div>
+          <Link to="/">
+            <button onClick={logout}>Logout</button>
+          </Link>
         </div>
       )}
-
-
-    
-     
     </div>
   );
 }
