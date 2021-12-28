9864d738d5fb8e1d14582261458245344f748ce9
BIT Student <classroom@bgit.rs>
2021-12-28 15:42:38 +0100

    login kompletiran

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useContext } from "react";
-import { Link} from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 import "./login.css";
 import { Dino } from "../../App";
 
@@ -7,18 +7,7 @@ function Login() {
   const [useremail, setUseremail] = useState("");
   const [password, setPassword] = useState("");
   const x = useContext(Dino);
-const[response, setResponse]=useState('')
-
-const showMessage=()=>{
-
-sessionStorage.setItem('token', response.accessToken)
-
-sessionStorage.getItem("token") === "undefined"
- ? x.setToken(null) : x.setToken(sessionStorage.getItem("token"));
-
-
-}
-
+  const [response, setResponse] = useState("");
 
   const loginButton = () => {
     fetch(`http://localhost:3333/login`, {
@@ -30,60 +19,72 @@ sessionStorage.getItem("token") === "undefined"
       }),
     })
       .then((res) => res.json())
-      .then(res=>setResponse(res))
-      // .then((res) => sessionStorage.setItem("token", res.accessToken))
-      // .then(() => {
-      //   sessionStorage.getItem("token") === "undefined"
-      //   ? x.setToken(null)
-      //   : x.setToken(sessionStorage.getItem("token"));
-      // })
-      
+      .then((res) => {
+        setResponse(res);
+        sessionStorage.setItem("token", res.accessToken);
+        sessionStorage.getItem("token") === "undefined"
+          ? x.setToken(null)
+          : x.setToken(sessionStorage.getItem("token"));
+      });
+    // .then((res) => sessionStorage.setItem("token", res.accessToken))
+    // .then(() => {
+    //   sessionStorage.getItem("token") === "undefined"
+    //   ? x.setToken(null)
+    //   : x.setToken(sessionStorage.getItem("token"));
+    // // })
   };
 
   return (
     <div className="loginpage">
-
       <div className="login-content">
-      <div className="login-field">
-      <p>Username</p>
-      <input className="login-input"
-        type="text"
-        placeholder="Username"
-        onChange={(u) => {
-          setUseremail(u.target.value);
-        }}
-      />
-</div>
-<p className="login-message">{response==="Cannot find user"?response:''}
-{response==="Email format is invalid"? response:''}</p>
-      
-      <div className="login-field">
-      <p >Password</p>
-      <input className="login-input"
-        type="password"
-        name=""
-        id=""
-        placeholder="Password"
-        onChange={(p) => {
-          setPassword(p.target.value);
-        }}
-      />
+        <div className="login-field">
+          <p>Username</p>
+          <input
+            className="login-input"
+            type="text"
+            placeholder="Username"
+            onChange={(u) => {
+              setUseremail(u.target.value);
+            }}
+          />
+        </div>
+        <p className="login-message">
+          {response === "Cannot find user" ? response : ""}
+          {response === "Email format is invalid" ? response : ""}
+        </p>
 
-      </div>
-      <p className="login-message">{response==="Incorrect password"? response:''}
-      {response==="Email and password are required"?response:''}
-      {response==="Password is too short"?response:''}</p>
+        <div className="login-field">
+          <p>Password</p>
+          <input
+            className="login-input"
+            type="password"
+            name=""
+            id=""
+            placeholder="Password"
+            onChange={(p) => {
+              setPassword(p.target.value);
+            }}
+          />
+        </div>
+        <p className="login-message">
+          {response === "Incorrect password" ? response : ""}
+          {response === "Email and password are required" ? response : ""}
+          {response === "Password is too short" ? response : ""}
+        </p>
 
-      
-      <div className="login-buttons">
-      <button className="login-button" onClick={()=>{loginButton(); 
-        showMessage()}}>
-        LOG IN
-      </button>
-      <Link to="/">
-        <button className="login-button">BACK</button>
-      </Link>
-      </div>
+        <div className="login-buttons">
+          <button
+            className="login-button"
+            onClick={() => {
+              loginButton();
+            }}
+          >
+            LOG IN
+          </button>
+          <Link to="/">
+            <button className="login-button">BACK</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
