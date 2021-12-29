701cef4136b4fa7ab6c28782c4e199e2ae018b23
Ivana <ivaptt@gmail.com>
2021-12-29 05:28:21 +0100

    finished stylizing the LoginPage

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -22,9 +22,7 @@ function Login() {
       .then((res) => {
         setResponse(res);
         sessionStorage.setItem("token", res.accessToken);
-        sessionStorage.getItem("token") === "undefined"
-          ? x.setToken(null)
-          : x.setToken(sessionStorage.getItem("token"));
+        sessionStorage.getItem("token") === "undefined" ? x.setToken(null) : x.setToken(sessionStorage.getItem("token"));
       });
     // .then((res) => sessionStorage.setItem("token", res.accessToken))
     // .then(() => {
@@ -73,17 +71,16 @@ function Login() {
         </p>
 
         <div className="login-buttons">
+          <Link to="/">
+            <button className="login-button">BACK</button>
+          </Link>
           <button
             className="login-button"
             onClick={() => {
               loginButton();
-            }}
-          >
+            }}>
             LOG IN
           </button>
-          <Link to="/">
-            <button className="login-button">BACK</button>
-          </Link>
         </div>
       </div>
     </div>
